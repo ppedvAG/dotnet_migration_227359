2070e763b97cf07aa2ced7259b94b8c2ed7d422e
André <AndreR@ppedv.de>
2023-04-19 11:53:59 +0200

    Linq ist cool

---
+++ HalloLinq/HalloLinq/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace HalloLinq
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            dataGridView1 = new DataGridView();
+            button6 = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.BackColor = Color.PaleTurquoise;
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Controls.Add(button5);
+            flowLayoutPanel1.Controls.Add(button6);
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1024, 96);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Alle Zeigen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button2.Location = new Point(154, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(359, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Filter nach \"R\" (Linq Expression)";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button3.Location = new Point(519, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(331, 42);
+            button3.TabIndex = 2;
+            button3.Text = "Filter nach \"R\" (Linq Lambda)";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.AutoSize = true;
+            button4.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button4.Location = new Point(3, 51);
+            button4.Name = "button4";
+            button4.Size = new Size(190, 42);
+            button4.TabIndex = 3;
+            button4.Text = "Average Month";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.AutoSize = true;
+            button5.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button5.Location = new Point(199, 51);
+            button5.Name = "button5";
+            button5.Size = new Size(149, 42);
+            button5.TabIndex = 4;
+            button5.Text = "First im Mai";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 96);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.RowTemplate.Height = 41;
+            dataGridView1.Size = new Size(1024, 354);
+            dataGridView1.TabIndex = 1;
+            // 
+            // button6
+            // 
+            button6.AutoSize = true;
+            button6.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button6.Location = new Point(354, 51);
+            button6.Name = "button6";
+            button6.Size = new Size(88, 42);
+            button6.TabIndex = 5;
+            button6.Text = "Select";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ HalloLinq/HalloLinq/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HalloLinq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            employees.Add(new Employee() { Id = 1, FirstName = "Fred", LastName = "Feuerstein", BirthDate = new DateTime(1984, 4, 7) });
+            employees.Add(new Employee() { Id = 2, FirstName = "Wilma", LastName = "Flintstone", BirthDate = new DateTime(1985, 5, 15) });
+            employees.Add(new Employee() { Id = 3, FirstName = "Barney", LastName = "Rubble", BirthDate = new DateTime(1986, 6, 23) });
+            employees.Add(new Employee() { Id = 4, FirstName = "Betty", LastName = "Rubble", BirthDate = new DateTime(1987, 7, 31) });
+            employees.Add(new Employee() { Id = 5, FirstName = "Bam Bam", LastName = "Rubble", BirthDate = new DateTime(1988, 8, 8) });
+            employees.Add(new Employee() { Id = 6, FirstName = "Dino", LastName = "Flintstone", BirthDate = new DateTime(1989, 9, 20) });
+            employees.Add(new Employee() { Id = 7, FirstName = "Mr.", LastName = "Slate", BirthDate = new DateTime(1990, 10, 1) });
+            employees.Add(new Employee() { Id = 8, FirstName = "Pearl", LastName = "Slaghoople", BirthDate = new DateTime(1991, 11, 3) });
+            employees.Add(new Employee() { Id = 9, FirstName = "Joe", LastName = "Rockhead", BirthDate = new DateTime(1992, 12, 5) });
+            employees.Add(new Employee() { Id = 10, FirstName = "The Great", LastName = "Gazoo", BirthDate = new DateTime(1993, 1, 7) });
+            employees.Add(new Employee() { Id = 11, FirstName = "Samantha", LastName = "Slaghoople", BirthDate = new DateTime(1994, 2, 9) });
+            employees.Add(new Employee() { Id = 12, FirstName = "George", LastName = "Slate", BirthDate = new DateTime(1995, 3, 11) });
+            employees.Add(new Employee() { Id = 13, FirstName = "Zonk", LastName = "Rubble", BirthDate = new DateTime(1996, 4, 13) });
+            employees.Add(new Employee() { Id = 14, FirstName = "Shmoo", LastName = "Slaghoople", BirthDate = new DateTime(1997, 5, 15) });
+            employees.Add(new Employee() { Id = 15, FirstName = "Rocky", LastName = "Gravel", BirthDate = new DateTime(1998, 6, 17) });
+            employees.Add(new Employee() { Id = 16, FirstName = "Crush", LastName = "Slaghoople", BirthDate = new DateTime(1999, 7, 19) });
+            employees.Add(new Employee() { Id = 17, FirstName = "Pebbles", LastName = "Flintstone", BirthDate = new DateTime(2000, 8, 21) });
+            employees.Add(new Employee() { Id = 18, FirstName = "Roxy", LastName = "Rockhead", BirthDate = new DateTime(2001, 9, 23) });
+            employees.Add(new Employee() { Id = 19, FirstName = "Chip", LastName = "Rockhead", BirthDate = new DateTime(2002, 10, 25) });
+        }
+
+        List<Employee> employees = new List<Employee>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = employees;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var query = from emp in employees
+                        where emp.LastName.StartsWith("R")
+                        orderby emp.LastName, emp.FirstName descending
+                        select emp;
+
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = employees.Where(x => x.LastName.StartsWith("R"))
+                                                .OrderBy(x => x.LastName)
+                                                .ThenByDescending(x => x.FirstName)
+                                                .ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{employees.Average(x => x.BirthDate.Month)} ⌀ Monat");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var emp = employees.FirstOrDefault(x => x.BirthDate.Month == 5);
+
+            if (emp != null)
+                MessageBox.Show($"{emp.FirstName}");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var query = from emp in employees
+                        select new { Name = $"{emp.FirstName} {emp.LastName}", BDate = emp.BirthDate };
+
+            dataGridView1.DataSource = query.ToList();
+        }
+    }
+
+    static class DateTimeKW
+    {
+        public static int GetKw(this DateTime dt) //extention Method
+        {
+            return 16;
+        }
+    }
+}
